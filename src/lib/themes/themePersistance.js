27fa19431fcad749f12e5094fbdd7f901e49af4e
jessie <codelikecraze@gmail.com>
2023-06-23 10:53:43 -0600

    change: make dark mode valid

--- src/lib/themes/themePersistance.js
+++ src/lib/themes/themePersistance.js
@@ -1,12 +1,12 @@
 import cookie from 'cookie';
 
-import {DEFAULT_THEME, HIGH_CONTRAST_THEME} from '.';
+import {DEFAULT_THEME, HIGH_CONTRAST_THEME, DARK_THENE} from '.';
 
 const PREFERS_HIGH_CONTRAST_QUERY = '(prefers-contrast: more)';
 const COOKIE_KEY = 'scratchtheme';
 
-// Dark mode isn't enabled yet
-const isValidTheme = theme => [DEFAULT_THEME, HIGH_CONTRAST_THEME].includes(theme);
+// Dark mode is enabled now :)
+const isValidTheme = theme => [DEFAULT_THEME, HIGH_CONTRAST_THEME, DARK_THENE].includes(theme);
 
 const systemPreferencesTheme = () => {
     if (window.matchMedia && window.matchMedia(PREFERS_HIGH_CONTRAST_QUERY).matches) return HIGH_CONTRAST_THEME;
